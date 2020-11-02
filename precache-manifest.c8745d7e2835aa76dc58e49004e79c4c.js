2878cab59fc34db7d56afc83c44cd04edd4f1f13
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-03 04:26:26 +0500

    Updates

---
+++ precache-manifest.c8745d7e2835aa76dc58e49004e79c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64fedf3e6ead89d8cdfa986aebc7b1e",
+    "url": "/foto-flick/index.html"
+  },
+  {
+    "revision": "0aa33311414fde22941a",
+    "url": "/foto-flick/static/css/2.344b70e2.chunk.css"
+  },
+  {
+    "revision": "912bcc4d6efb249ce581",
+    "url": "/foto-flick/static/css/main.5f9d4977.chunk.css"
+  },
+  {
+    "revision": "0aa33311414fde22941a",
+    "url": "/foto-flick/static/js/2.4e853281.chunk.js"
+  },
+  {
+    "revision": "246ad901f08703825af5eeab9ed7a0eb",
+    "url": "/foto-flick/static/js/2.4e853281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912bcc4d6efb249ce581",
+    "url": "/foto-flick/static/js/main.6ad0a995.chunk.js"
+  },
+  {
+    "revision": "1c35de86f05687eb5af0",
+    "url": "/foto-flick/static/js/runtime-main.67d19f07.js"
+  }
+]);
